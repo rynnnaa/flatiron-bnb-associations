469727a6a538e616e2528ba4908018ee2e9c304a
Ryna <pinktulips@Hotmail.com>
2021-07-23 08:18:00 -0700

    d

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
 class Listing < ActiveRecord::Base
   belongs_to :neighborhood
-  
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
+  has_many :listings, { foreign_key: 'host_id' }
 end
